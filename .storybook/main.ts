ca4206b9031831cef10cfca88a33573f56c07a31
cylewaitforit <cyle@cylewaitfor.it>
2026-04-04 20:02:24 -0500

    refactor: move to `.storybook/docs`

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,7 +1,7 @@
 import type { StorybookConfig } from '@storybook-vue/nuxt'
 
 const config = {
-  stories: ['../.storybook/*.mdx', '../app/**/*.@(mdx|stories.@(js|ts))'],
+  stories: ['../.storybook/docs/*.mdx', '../app/**/*.@(mdx|stories.@(js|ts))'],
   addons: [
     '@storybook/addon-a11y',
     '@storybook/addon-docs',
