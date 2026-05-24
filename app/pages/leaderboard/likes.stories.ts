6f549b76a9f87f1d27071844af82a7dfa6ff2d93
cylewaitforit <cyle@cylewaitfor.it>
2026-05-24 14:08:46 -0500

    fix: simplify podium only handler

--- app/pages/leaderboard/likes.stories.ts
+++ app/pages/leaderboard/likes.stories.ts
@@ -2,7 +2,10 @@ import LikesLeaderboard from './likes.vue'
 import type { Meta, StoryObj } from '@storybook-vue/nuxt'
 import { http, HttpResponse } from 'msw'
 import { pageDecorator } from '../../../.storybook/decorators'
-import { likesLeaderboardHandler } from '../../storybook/mocks/handlers/leaderboard-likes'
+import {
+  likesLeaderboardHandler,
+  likesLeaderboardEntries,
+} from '../../storybook/mocks/handlers/leaderboard-likes'
 
 const meta = {
   component: LikesLeaderboard,
@@ -27,54 +30,7 @@ export const PodiumOnly: Story = {
     msw: {
       handlers: [
         http.get('/api/leaderboard/likes', () =>
-          HttpResponse.json([
-            {
-              rank: 1,
-              packageName: 'vite',
-              subjectRef: 'at://did:plc:mock0001/dev.npmx.like/3jui7kd452c2a',
-              totalLikes: 4821,
-              packageDescription: 'Next generation frontend tooling. It is fast!',
-              weeklyDownloads: 12500000,
-              repositoryStars: 68000,
-              homepagePreviewUrl: 'https://placehold.co/1200x628/png',
-              homepagePreviewWidth: 1200,
-              homepagePreviewHeight: 628,
-              homepageLogoUrl: null,
-              homepageLogoWidth: null,
-              homepageLogoHeight: null,
-            },
-            {
-              rank: 2,
-              packageName: 'react',
-              subjectRef: 'at://did:plc:mock0001/dev.npmx.like/3jui7kd452c2b',
-              totalLikes: 3204,
-              packageDescription: 'React is a JavaScript library for building user interfaces.',
-              weeklyDownloads: 55000000,
-              repositoryStars: 228000,
-              homepagePreviewUrl: null,
-              homepagePreviewWidth: null,
-              homepagePreviewHeight: null,
-              homepageLogoUrl: null,
-              homepageLogoWidth: null,
-              homepageLogoHeight: null,
-            },
-            {
-              rank: 3,
-              packageName: '@nuxt/kit',
-              subjectRef: 'at://did:plc:mock0001/dev.npmx.like/3jui7kd452c2c',
-              totalLikes: 2187,
-              packageDescription:
-                'Nuxt Kit provides composables and utilities for Nuxt module authors.',
-              weeklyDownloads: 3200000,
-              repositoryStars: 55000,
-              homepagePreviewUrl: null,
-              homepagePreviewWidth: null,
-              homepagePreviewHeight: null,
-              homepageLogoUrl: null,
-              homepageLogoWidth: null,
-              homepageLogoHeight: null,
-            },
-          ]),
+          HttpResponse.json(likesLeaderboardEntries.slice(0, 3)),
         ),
       ],
     },
